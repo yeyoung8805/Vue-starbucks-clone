dcfb1572e51f158cdc0ad5bed8cf82d814523b4d
yeyoung8805 <yyk8805@naver.com>
2022-07-14 16:24:12 +0900

    add opacity using gsap library

--- js/main.js
+++ js/main.js
@@ -23,9 +23,14 @@ window.addEventListener('scroll', _.throttle(function() {
   console.log(window.scrollY);
   if(window.scrollY > 500) {
     //배지 숨기기
-    badgeEl.style.display = 'none';
+    // gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0
+    });
   }else {
     //배지 보이기
-    badgeEl.style.display = 'block';
+    gsap.to(badgeEl, .6, {
+      opacity: 1
+    });
   }
 }, 300));
