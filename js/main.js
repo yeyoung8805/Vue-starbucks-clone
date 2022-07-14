c4da0af803c1831ae2069e329ba32be481c0c252
yeyoung8805 <yyk8805@naver.com>
2022-07-14 09:33:44 +0900

    apply focus/blur event to search icon

--- js/main.js
+++ js/main.js
@@ -5,3 +5,13 @@ const searchInputEl = searchEl.querySelector('input');
 searchEl.addEventListener('click', function() {
   searchInputEl.focus();
 });
+
+searchInputEl.addEventListener('focus', function() {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function() { //blur 는 focus 이벤트의 반대
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});
