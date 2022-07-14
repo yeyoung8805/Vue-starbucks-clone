13244460f864b00a51a5ee2b4e661816edc9bf8e
yeyoung8805 <yyk8805@naver.com>
2022-07-14 16:27:19 +0900

    add display none, block to the gsap

--- js/main.js
+++ js/main.js
@@ -25,12 +25,14 @@ window.addEventListener('scroll', _.throttle(function() {
     //배지 숨기기
     // gsap.to(요소, 지속시간, 옵션);
     gsap.to(badgeEl, .6, {
-      opacity: 0
+      opacity: 0,
+      display: 'none'
     });
   }else {
     //배지 보이기
     gsap.to(badgeEl, .6, {
-      opacity: 1
+      opacity: 1,
+      display: 'block'
     });
   }
 }, 300));
