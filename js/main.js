4dc75618c638ddf18b50d22781deb011f0103400
yeyoung8805 <yyk8805@naver.com>
2022-07-15 15:51:02 +0900

    swiper slide and btn

--- js/main.js
+++ js/main.js
@@ -57,9 +57,9 @@ new Swiper('.notice-line .swiper-container', {
 new Swiper('.promotion .swiper-container', {
   slidesPerView: 3, //한번에 보여줄 슬라이드 개수
   spaceBetween: 10, //슬라이드 사이 여백
-  centerSlides: true, //1번 슬라이드가 가운데 보이기
+  centeredSlides: true, //1번 슬라이드가 가운데 보이기
   loop: true, //반복재생
-  autoplay: {
-    delay: 5000
-  }
+  // autoplay: {
+  //   delay: 5000
+  // }
 });
